96b42acd7615f572425c98157bbf9869c6f909b3
Anshu Bantra <anshubantra@gmail.com>
2024-03-23 15:18:08 +1100

    20240323_001

---
+++ Developer/012_Writing_Functions_in_Python/best_practises.py
@@ -0,0 +1,270 @@
+# %% # Add a docstring to count_letter()
+def count_letter(content, letter):
+  """
+  Count the number of times `letter` appears in `content`.
+
+  Args:
+    content (str): The string to search.
+    letter (str): The letter to search for.
+
+  Returns:
+    int
+
+  # Add a section detailing what errors might be raised
+  Raises:
+    ValueError: If `letter` is not a one-character string.
+  """
+  if (not isinstance(letter, str)) or len(letter) != 1:
+    raise ValueError('`letter` must be a single character string.')
+  return len([char for char in content if char == letter])
+
+#%%  # Get the "count_letter" docstring by using an attribute of the function
+docstring = count_letter.__doc__
+
+border = '#' * 28
+print('{}\n{}\n{}'.format(border, docstring, border))
+
+# %%  # Inspect the count_letter() function to get its docstring
+import inspect
+
+docstring = inspect.getdoc(count_letter)
+
+border = '#' * 28
+print('{}\n{}\n{}'.format(border, docstring, border))
+
+# %%  # build_tooltip
+import inspect
+
+def build_tooltip(function):
+  """Create a tooltip for any function that shows the
+  function's docstring.
+
+  Args:
+    function (callable): The function we want a tooltip for.
+
+  Returns:
+    str
+  """
+  # Get the docstring for the "function" argument by using inspect
+  docstring = inspect.getdoc(function)
+  border = '#' * 28
+  return '{}\n{}\n{}'.format(border, docstring, border)
+
+print(build_tooltip(count_letter))
+print(build_tooltip(range))
+print(build_tooltip(print))
+
+# %%  # DRY & DOT   ==> Don't Repeat Yourself and Do One Thing
+import pandas
+
+# Use an immutable variable for the default argument
+def better_add_column(values, df=None):
+  """Add a column of `values` to a DataFrame `df`.
+  The column will be named "col_<n>" where "n" is
+  the numerical index of the column.
+
+  Args:
+    values (iterable): The values of the new column
+    df (DataFrame, optional): The DataFrame to update.
+      If no DataFrame is passed, one is created by default.
+
+  Returns:
+    DataFrame
+  """
+  # Update the function to create a default DataFrame
+  if df is None:
+    df = pandas.DataFrame()
+  df['col_{}'.format(len(df.columns))] = values
+  return df
+
+# %%  Context Managers
+# Add a decorator that will make timer() a context manager
+import contextlib
+import time
+
+@contextlib.contextmanager
+def timer():
+  """Time the execution of a context block.
+
+  Yields:
+    None
+  """
+  start = time.time()
+  # Send control back to the context block
+  yield
+  end = time.time()
+  print('Elapsed: {:.2f}s'.format(end - start))
+
+with timer():
+  print('This should take approximately 0.25 seconds')
+  time.sleep(0.25)
+# %%   
+
+@contextlib.contextmanager
+def open_read_only(filename):
+  """Open a file in read-only mode.
+
+  Args:
+    filename (str): The location of the file to read
+
+  Yields:
+    file object
+  """
+  read_only_file = open(filename, mode='r')
+  # Yield read_only_file so it can be assigned to my_file
+  yield read_only_file
+  # Close read_only_file
+  read_only_file.close()
+
+with open_read_only('my_file.txt') as my_file:
+  print(my_file.read())
+# %%  # Decorators
+
+# Add the missing function references to the function map
+  import numpy as np
+function_map = {
+  'sum': np.sum,
+  'mean': np.mean,
+  'std': np.std,
+  'minimum': np.min,
+  'maximum': np.max
+}
+
+data = [range(20)]
+print(data)
+
+func_name = input("'sum', 'mean', 'std', 'minimum', 'maximum'  ")
+
+# Call the chosen function and pass "data" as an argument
+function_map[func_name](data)
+# %%
+
+def create_math_function(func_name):
+  if func_name == 'add':
+    def add(a, b):
+      return a + b
+    return add
+  elif func_name == 'subtract':
+    # Define the subtract() function
+    def subtract(a, b):
+      return a-b
+    return subtract
+  else:
+    print("I don't know that one")
+    
+add = create_math_function('add')
+print('5 + 2 = {}'.format(add(5, 2)))
+
+subtract = create_math_function('subtract')
+print('5 - 2 = {}'.format(subtract(5, 2)))
+# %%
+
+x = 50
+
+def one():
+  x = 10
+
+def two():
+  global x
+  x = 30
+
+def three():
+  x = 100
+  print(x)
+
+for func in [one, two, three]:
+  func()
+  print(x)
+
+# %%   # Closures
+
+def foo():
+  a = 5
+  def bar():
+    print(a)
+  return bar
+
+func = foo()
+func()
+
+print(len(func.__closure__))
+print(func.__closure__[0].cell_contents)
+# %%
+
+def return_a_func(arg1, arg2):
+  def new_func():
+    print('arg1 was {}'.format(arg1))
+    print('arg2 was {}'.format(arg2))
+  return new_func
+    
+my_func = return_a_func(2, 17)
+
+# Show that my_func()'s closure is not None
+print(my_func.__closure__ is not None)
+print(len(my_func.__closure__))
+for _ in range(len(my_func.__closure__)):
+  print(_+1, my_func.__closure__[_].cell_contents)
+# %%
+
+def my_special_function():
+  print('You are running my_special_function()')
+  
+def get_new_func(func):
+  def call_func():
+    func()
+  return call_func
+
+new_func = get_new_func(my_special_function)
+
+# Redefine my_special_function() to just print "hello"
+def my_special_function():
+  print('hello')
+
+new_func()
+# %%
+
+def my_special_function():
+  print('You are running my_special_function()')
+  
+def get_new_func(func):
+  def call_func():
+    func()
+  return call_func
+
+new_func = get_new_func(my_special_function)
+
+# Delete my_special_function()
+del( my_special_function )
+
+new_func()
+# %%
+
+def my_special_function():
+  print('You are running my_special_function()')
+  
+def get_new_func(func):
+  def call_func():
+    func()
+  return call_func
+
+# Overwrite `my_special_function` with the new function
+my_special_function = get_new_func(my_special_function)
+
+my_special_function()
+# %%    # Decorators
+
+def multiply(a, b):
+  return a*b
+
+def double_args(func):
+  def wrapper(a, b):
+    return func(a*2,b*2)
+  return wrapper
+
+print(multiply(1,5))
+d_m = double_args(multiply)
+print(d_m(1,5))
+
+# %%
+
+
