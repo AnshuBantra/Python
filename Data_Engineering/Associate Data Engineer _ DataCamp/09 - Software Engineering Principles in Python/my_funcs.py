a32eaeee5bb62e1d899d425e6cba44beb7dccd59
Anshu Bantra <anshubantra@gmail.com>
2024-08-30 11:38:05 +1000

    20240830_001

---
+++ Data_Engineering/Associate Data Engineer _ DataCamp/09 - Software Engineering Principles in Python/my_funcs.py
@@ -0,0 +1,13 @@
+def square(x):
+    """ Square the number x
+    :param x: number to square
+    :return: x squared
+
+    >>> square(3)
+    9
+    """
+
+    return x**2
+
+import doctest
+doctest.testmod()
