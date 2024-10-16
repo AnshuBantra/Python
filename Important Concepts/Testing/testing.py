8d1fe20b1e10daa6e099db9d243c45b7ad0687d6
Anshu Bantra <anshubantra@gmail.com>
2024-10-16 14:02:44 +1100

    20241016_002

---
+++ Important Concepts/Testing/testing.py
@@ -0,0 +1,13 @@
+# https://www.youtube.com/watch?v=F60iJ3xxgtg
+
+def add_numbers(a: int, b: int):
+    return a+b
+
+add_numbers(1, 2)
+
+
+lst = [1, 2, 3, 4, 5]
+
+lst1 = [[1,2], [3,4]]
+
+add_numbers('5', 5)
