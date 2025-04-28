81a848d3b3af2757870b0f7740d600c4ecbc6799
Anshu Bantra <anshubantra@gmail.com>
2025-04-28 18:55:27 +1000

    20250427_002

--- Ultimate Data Science with Gen AI/01-Python_Basics/1-Basics.py
+++ Ultimate Data Science with Gen AI/01-Python_Basics/1-Basics.py
@@ -1,8 +1,20 @@
 #%%
-print(1+1)
+def safe_int(num):
+    try:
+        num = int(num)
+    except ValueError:
+        print(f'Cannot convert {num} to integer, resetting to 0')
+        return 0
+    return num
 
-# %%
-a = 1
-print(a)
+val_1 = safe_int(input('Enter a number (Value 1) : '))
+val_2 = safe_int(input('Enter a number (Value 2) : '))
+
+def add(val_1, val_2):
+    c = val_1 + val_2
+    print(f'{c=}')
+    return c
+
+print(add(val_1, val_2))
 
 # %%
